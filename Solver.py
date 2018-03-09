7d136ca42b94ec8fc80d6ced46b9de3e5813bf1d
Ed Jeffery <edrjeffery@gmail.com>
2018-03-09 18:25:33 +0000

    Creates list for possible values for each cell in sudoku board

--- Solver.py
+++ Solver.py
@@ -18,25 +18,42 @@ class Solver:
                 It contains the solution, if there is one. If there is no solution, all array entries should be -1.
         """
 
-        for row in range(0,9):
-            r = sudoku[row]
-            print(r)
+        board = np.copy(sudoku)
+
+        for row in range(0, 9):
+            r = board[row]
             count = np.count_nonzero(r == 0)
-            print(count)
 
-        for col in range(0,9):
-            c = sudoku[:, col]
-            print(c)
+        for col in range(0, 9):
+            c = board[:, col]
             count = np.count_nonzero(c == 0)
-            print(count)
 
-        for i in range(0,9):
-            row = sudoku[row]
+        for i in range(0, 9):
+            row = board[i]
             for j in range(0,9):
-                col = sudoku[:, j]
+                print(board[i, j])
+                col = board[:, j]
+                row_low = (i // 3) * 3
+                row_high = row_low + 3
+                col_low = (j // 3) * 3
+                col_high = col_low + 3
+                #print(row_low, row_high, col_low, col_high)
+                grid = board[row_low:row_high, col_low:col_high]
+                #print("Row: ", row)
+                #print("Col: ", col)
+                #print(grid)
+
+                possible_values = []
+                for x in range(1,10):
+                    if x not in row:
+                        if x not in col:
+                            if x not in grid:
+                                possible_values.append(x)
+                print(i, j, possible_values)
+
                 
 
-        solved_sudoku = sudoku
+        solved_sudoku = sudoku #deep copy
 
 
         return solved_sudoku
